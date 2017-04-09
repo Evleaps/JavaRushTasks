bd2ba471f79128d31f51250b25ecdab19f5571ee
Ромчи <r.aymalet@mail.ru>
2017-04-09 20:28:42 +0300

    new task

--- 2.JavaCore/src/com/javarush/task/task16/task1632/Message.java
+++
@@ -1,5 +0,0 @@
-package com.javarush.task.task16.task1632;
-
-public interface Message {
-    void showWarning();
-}
---
+++ 2.JavaCore/src/com/javarush/task/task16/task1632_5разныхМетодов/Message.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task16.task1632_5разныхМетодов;
+
+public interface Message {
+    void showWarning();
+}
